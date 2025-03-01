73e81e070e15fd14088505d14c40c5670fd0d36c
ArturPuhaev <artpuhaev@gmail.com>
2025-03-01 08:55:34 +0300

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        int AccountNumber = 100;
-        int AddingNumber = 1200;
-        int Balance = AccountNumber + AddingNumber;
-        int BonusRuble = AddingNumber / 100;
-        int FinalBalance = Balance + BonusRuble;
-        if (AddingNumber > 1000) {
-            System.out.println("Ваш баланс " + FinalBalance);
-            System.out.println("Вам начислено бонусных рублей " + BonusRuble);
+        int accountNumber = 100;
+        int addingNumber = 1200;
+        int balance = accountNumber + addingNumber;
+        int bonusRuble = addingNumber / 100;
+        int finalBalance = balance + bonusRuble;
+        if (addingNumber > 1000) {
+            System.out.println("Ваш баланс " + finalBalance);
+            System.out.println("Вам начислено бонусных рублей " + bonusRuble);
 
         } else {
-            System.out.println("Ваш баланс " + Balance);
+            System.out.println("Ваш баланс " + balance);
         }
 
     }
